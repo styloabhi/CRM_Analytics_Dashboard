775a84e1de9134e857642eaae6a29a08703515c0
Abhishek Kumar Pandey <styloabhi@gmail.com>
2025-12-23 14:51:17 +0530

    Small Update

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ accounts        = pd.read_csv("Resources/accounts.csv")
 sales_pipeline  = pd.read_csv("Resources/sales_pipeline.csv")
 product_360     = pd.read_csv("Resources/product_360.csv")
 sales_agent_360 = pd.read_csv("Resources/sales_agent_360.csv")
-cohort_raw      = pd.read_csv("CResources/cohort_raw.csv")
+cohort_raw      = pd.read_csv("Resources/cohort_raw.csv")
 account_360     = pd.read_csv("Resources/account_360.csv")
 
 
